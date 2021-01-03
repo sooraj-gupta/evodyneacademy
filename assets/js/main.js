dba3042a1111462c677d2a5dec2ad015b511952f
sooraj-gupta <soorajgupta04@gmail.com>
2021-01-03 13:42:33 -0800

    added evoarm page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,9 @@
+if ( localStorage.getItem("jan2021warn") != "closed" )
+{
+	document.getElementById( "jan2021warn" ).style.display = "table";
+}
+document.documentElement.style.setProperty("--color", "#f90" );
+
 var open = false;
 document.getElementById("ham").onclick = function()
 {
@@ -50,11 +56,11 @@ document.getElementsByClassName("menu")[0].onclick = function()
 	}
 }
 
-document.getElementById("color").addEventListener( "keydown", event =>
-{
-	if( event.keyCode == 13 )
-		document.documentElement.style.setProperty("--color", ""+ document.getElementById("color").value );
-});
+//document.getElementById("color").addEventListener( "keydown", event =>
+//{
+//	if( event.keyCode == 13 )
+//		document.documentElement.style.setProperty("--color", ""+ document.getElementById("color").value );
+//});
 
 for( var i = 0; i < document.getElementsByClassName("horizmenuitem").length; i++ )
 {
@@ -75,25 +81,41 @@ function changeHorizMenuElement( el )
 
 let fhs = document.getElementsByClassName("faqheader");
 let lastFH = "#forparents";
-setInterval( ()=>{
-	
-	if( document.body.id == "faq" )
-	{
-		for( var i = 0; i < fhs.length; i++ )
-		{
-			if( fhs[i].getBoundingClientRect().top < 20 )
+if( document.body.id == "faq" )
+{
+	setInterval( ()=>{
+			for( var i = 0; i < fhs.length; i++ )
 			{
-				if( lastFH != document.getElementsByClassName("horizmenuitem")[i].getAttribute( "href" ) ) 
+				if( fhs[i].getBoundingClientRect().top < 20 )
 				{
-					if( i < document.getElementsByClassName("horizmenuitem").length )
+					if( lastFH != document.getElementsByClassName("horizmenuitem")[i].getAttribute( "href" ) ) 
 					{
-						changeHorizMenuElement( document.getElementsByClassName("horizmenuitem")[i] )
-						lastFH = document.getElementsByClassName("horizmenuitem")[i].getAttribute( "href" );
+						if( i < document.getElementsByClassName("horizmenuitem").length )
+						{
+							changeHorizMenuElement( document.getElementsByClassName("horizmenuitem")[i] )
+							lastFH = document.getElementsByClassName("horizmenuitem")[i].getAttribute( "href" );
+						}
+						console.log( fhs[i] );
 					}
-					console.log( fhs[i] );
 				}
-			}
-		}
-	}
-	
-}, 300 );
+			}	
+	}, 300 );
+}
+if( document.body.id == "evoarm")
+{
+	let sidearm = document.getElementById("armfromside").getBoundingClientRect().top + window.scrollY;
+	let ak1 = document.getElementById("ak1").getBoundingClientRect().top + window.scrollY;
+	let ak2 = document.getElementById("ak2").getBoundingClientRect().top + window.scrollY;
+	let ak3 = document.getElementById("ak3").getBoundingClientRect().top + window.scrollY;
+	let margin = document.getElementById("armfromside").offsetHeight;
+	document.addEventListener( "scroll", () => 
+							 {
+			console.log ( Math.max ( ( ak1 - margin ) - window.scrollY  , 0 )  )
+			
+			
+				document.getElementById("armfromside").style.transform = "translateX( -"+Math.max ( ( ( sidearm - margin/(window.innerWidth/400) ) - window.scrollY )  , 0 )+"px )"
+				document.getElementById("ak1").style.transform = "translateX( "+Math.max ( ( ( ak1 - margin/(window.innerWidth/600)  ) - window.scrollY )  , 0 )+"px )"
+				document.getElementById("ak2").style.transform = "translateX( "+Math.max ( ( ( ak2 - margin/(window.innerWidth/600)  ) - window.scrollY )  , 0 )+"px )"
+				document.getElementById("ak3").style.transform = "translateX( "+Math.max ( ( ( ak3 - margin/(window.innerWidth/600)  ) - window.scrollY )  , 0 )+"px )"
+	});
+}
